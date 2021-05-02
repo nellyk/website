61473e80364f8b3053bbbc13345928f3eeea4d32
nellyk <nellykiboi@gmail.com>
2021-05-03 00:52:54 +0100

    feat(init): initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import '../styles/globals.css';
+import '../styles/tailwind.css';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider disableTransitionOnChange defaultTheme="dark">
+      <Component {...pageProps} />
+    </ThemeProvider>
+
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.jsx',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
